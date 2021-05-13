d4409d4bd38a1424df6adf8e26a008a6a647508e
linda930701 <linda930701@gmail.com>
2021-05-13 21:16:34 +0800

    new

--- p2/main.cpp
+++ p2/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
-using namespace std;
+#include<iomanip>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    float flt_Var=3.141592f;
+    int int_Var=1000.0f*flt_Var;
+    std::cout<< int_Var <<"\n";
+    float flt_Var2=int_Var/1000.0f;
+    std::cout<< std::setprecision(4) << flt_Var2 <<"\n";
     return 0;
 }
